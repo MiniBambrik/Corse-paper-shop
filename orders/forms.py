484139aa715e7880de967d8036ed65fd0cf07a9f
MiniBambrik <svitlana.zhmykhova@hneu.net>
2023-05-23 01:10:59 +0300

    withouterror

--- orders/forms.py
+++ orders/forms.py
@@ -6,13 +6,3 @@ class OrderCreateForm(forms.ModelForm):
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
     
-    def __init__(self, *args, **kwargs):
-        user = kwargs.pop('user', None)
-        super(OrderCreateForm, self).__init__(*args, **kwargs)
-        if user and user.is_authenticated:
-            self.fields['first_name'].initial = user.first_name
-            self.fields['last_name'].initial = user.last_name
-            self.fields['email'].initial = user.email
-            self.fields['city'].initial = user.city
-            self.fields['address'].initial = user.address
-            self.fields['postal_code'].initial = user.postal_code
--- orders/views.py
+++ orders/views.py
@@ -1,18 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from cart.cart import Cart
-from django.shortcuts import render, get_object_or_404
-
 # Create your views here.
-
 
 def order_create(request):
     cart = Cart(request)
     if request.method == 'POST':
         form = OrderCreateForm(request.POST)
         if form.is_valid():
-            order = form.save()
+            order = form.save(commit=False)
+            order.user = request.user
+            order.save()
             for item in cart:
                 OrderItem.objects.create(
                     order=order,
